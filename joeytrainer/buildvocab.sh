81044f0b1abb1efbb91fd2fb6edfa6b325a3e0a4
edAI <edai.official.edai@gmail.com>
2021-11-20 17:54:06 +0100

    additional commits for cluster

--- joeytrainer/buildvocab.sh
+++ joeytrainer/buildvocab.sh
@@ -2,6 +2,6 @@ src=en
 tgt=ig
 #mkdir -p joeynmt/data/frmos/fr_mos/10k/
 
-python joeynmt/scripts/build_vocab.py /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/train.$src /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/train.$tgt --output_path /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/vocab.txt
+python joeynmt/scripts/build_vocab.py /home/mila/c/chris.emezue/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/train.$src /home/mila/c/chris.emezue/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/train.$tgt --output_path /home/mila/c/chris.emezue/lafand-mt/joeytrainer/data/enig/spdata/10k/JW/vocab.txt
 
-python joeynmt/scripts/build_vocab.py /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/20k/JW/train.$src /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/20k/JW/train.$tgt --output_path /content/drive/MyDrive/lafand-ig/lafand-mt/joeytrainer/data/enig/spdata/20k/JW/vocab.txt
+python joeynmt/scripts/build_vocab.py /home/mila/c/chris.emezue/lafand-mt/joeytrainer/data/enig/spdata/20k/JW/train.$src /home/mila/c/chris.emezue/lafand-mt/joeytrainer/data/enig/spdata/20k/JW/train.$tgt --output_path /home/mila/c/chris.emezue/lafand-mt/joeytrainer/data/enig/spdata/20k/JW/vocab.txt
