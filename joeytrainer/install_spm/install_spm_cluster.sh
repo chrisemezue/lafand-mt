4baf380df1d33f21f1ec71a46e1ca4a928ef36d3
edAI <edai.official.edai@gmail.com>
2021-12-07 20:52:21 +0100

    additional commits

--- joeytrainer/install_spm/install_spm_cluster.sh
+++ joeytrainer/install_spm/install_spm_cluster.sh
@@ -5,4 +5,4 @@ cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=<PATH_TO_ENV>
 make -j $(nproc)
 make install
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:<PATH_TO_ENV>/env"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:<PATH_TO_ENV>/lib"
